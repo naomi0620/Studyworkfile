681857c0cf2c534bfad1d0b75fab31632e91b662
naomi0620 <m15033589525@163.com>
2018-11-12 23:25:22 +0800

    lily 80

--- 2.js
+++ 2.js
@@ -1,2 +1,20 @@
-var c=3;
-
+function Person(name){
+	this.name=name;
+}
+Person.prototype.sayHello=function(){
+	console.log("Helle"+this.name+"!");
+	
+}
+function Student(name,grade){
+	Person.call(this,name);
+	this.grade=grade;
+}
+(function(){
+	var Super=function(){};
+	Student.prototype=new Super();
+	Student.prototype.constructor=Student;
+})();
+Student.prototype.sayGrade=function(){
+	alert("mygrade:"+this.grade+".")
+}
+var a=new Student("lily","80");
